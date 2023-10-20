bc641c1e3b34fd83b2ef3dc9f6ab8133671b3128
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2023-10-20 14:30:51 +0200

    add simple select

--- jour_1.sql
+++ jour_1.sql
@@ -1,16 +1,22 @@
 -- 1. Créer des tables
+DROP TABLE books;
 CREATE TABLE books (
 	id INTEGER,
 	title VARCHAR(100),
 	year_published SMALLINT,
 	publisher VARCHAR(50),
-	price MONEY
+	price MONEY,
+	available  BOOL
 );
 
-
+DROP TABLE authors;
 CREATE TABLE authors (
 	id INTEGER,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50)
 );
+
+-- 2. Insertion simple
+INSERT INTO books(id, title, year_published, price, available) VALUES (1, 'Introduction au JAVA', 2020, '50', 'y');
+SELECT * FROM books;
 
